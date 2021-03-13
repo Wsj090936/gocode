27a8dd957187edc6875922e3293824d7175e1ff9
jiahao <jiahao@mogu.com>
2021-03-13 19:49:37 +0800

    channel close

--- BaseProject/src/ch7/channel/Channel_test.go
+++ BaseProject/src/ch7/channel/Channel_test.go
@@ -2,6 +2,7 @@ package channel
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 	"time"
 )
@@ -33,4 +34,54 @@ func OtherTask() {
 	fmt.Println("Other task start")
 	time.Sleep(time.Millisecond * 100)
 	fmt.Println("Other task complete")
+}
+
+
+func TestSelect(t *testing.T){
+	// 以下select 语句 case1 和case 2 会随机执行一个
+	select {
+	case ret := <- AsyncServide():
+		fmt.Println("执行了1:",ret)
+	case ret2 := <- AsyncServide():
+		fmt.Println("执行了2",ret2)
+	// 表示如果其他case超时了，就执行这条语句
+	case <-time.After(time.Millisecond * 100):
+
+		fmt.Println("失败了，执行后面吧")
+	// default 表示如果所有case都没准备好，就执行default
+	default:
+		fmt.Println("我是default")
+	}
+	fmt.Println("继续执行后面")
+}
+
+func TestChannelClose(t *testing.T) {
+	var ch = make(chan int)
+	var wg = sync.WaitGroup{}
+	wg.Add(1)
+	go doProducer(&wg,ch)
+	wg.Add(1)
+	go doReceiver(&wg,ch)
+	wg.Wait()
+}
+func doProducer(wg *sync.WaitGroup,ch chan int) {
+	for i:=0;i < 10;i++{
+		ch <- i
+	}
+	// 这里进行关闭通道
+	close(ch)
+	wg.Done()
+}
+
+func doReceiver(wg *sync.WaitGroup,ch chan int) {
+	for{
+		// 这里如果关闭了，就会进行关闭的广播
+		if data,ok := <- ch;ok{
+			fmt.Println(data)
+		}else {
+			fmt.Println("程序退出了")
+			break
+		}
+	}
+	wg.Done()
 }
